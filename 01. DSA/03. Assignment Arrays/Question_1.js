819233bac8335ae034c012f5a96859ba37626d61
architchaurasia <droidgraphix7@gmail.com>
2023-07-10 02:35:15 +0530

    update

---
+++ 01. DSA/03. Assignment Arrays/Question_1.js
@@ -0,0 +1,48 @@
+// Q.1 Given an integer array nums of length n and an integer target, find three integers
+// in nums such that the sum is closest to the target.
+// Return the sum of the three integers.
+
+// You may assume that each input would have exactly one solution.
+
+// Example 1:
+// Input: nums = [-1,2,1,-4], target = 1
+// Output: 2
+
+// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+//Solution
+function arrayNum(nums, target) {
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  let closestSum = Infinity;
+
+  for (let i = 0; i < n - 2; i++) {
+    let left = i + 1;
+    let right = n - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === target) {
+        return target;
+      }
+
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
+}
+
+const nums = [-1, 2, 1, -4];
+const target = 1;
+const result = arrayNum(nums, target);
+console.log(result); // Output: 2
